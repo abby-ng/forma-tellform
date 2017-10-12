e8c311d22989d0fc43035e66e3e40a86071b28ff
Arshad Ali Abdul Samad <arshadali@Arshads-MacBook-Pro.local>
2017-10-12 16:48:01 +0800

    added iscollabarator check

--- public/modules/forms/base/controllers/submit-form.client.controller.js
+++ public/modules/forms/base/controllers/submit-form.client.controller.js
@@ -2,10 +2,21 @@
 
 // SubmitForm controller
 angular.module('forms').controller('SubmitFormController', [
-	'$scope', '$rootScope', '$state', '$translate', 'myForm', 'Auth',
-	function($scope, $rootScope, $state, $translate, myForm, Auth) {
+	'$scope', '$rootScope', '$state', '$translate', 'myForm', 'Auth', 'User',
+	function($scope, $rootScope, $state, $translate, myForm, Auth, user) {
 		$scope.authentication = Auth;
 		$scope.myform = myForm;
+
+        
+		$rootScope.isCollaborator = function() {
+			$scope.user = $rootScope.user = Auth.ensureHasCurrentUser(user);
+            if ($scope.user && (($scope.myform.admin._id === $scope.user._id) || ($scope.myform.collaborators.indexOf($scope.user.email)))) {
+                return true;
+            } else {
+                return false
+            }
+
+        }
 
 		$translate.use(myForm.language);
 	}
--- public/modules/users/config/users.client.config.js
+++ public/modules/users/config/users.client.config.js
@@ -7,9 +7,9 @@ angular.module('users').config(['$httpProvider',
       return {
         responseError: function(response) {
           if (response.status === 401) {
-            // $window.location.assign('/#!/signin');
+            $window.location.assign('/#!/signin');
           } else if(response.status === 403){
-            // $window.location.assign('/#!/access_denied');
+            $window.location.assign('/#!/forms');
           }
 
           return $q.reject(response);
